b13d1d0411671ce7c1a56705b0a91c0954e7a0be
TTTT <40955281+tatikun@users.noreply.github.com>
2020-05-14 15:08:16 +0900

    Initial commit

---
+++ Assets/CVCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class CVCamera : MonoBehaviour
+{
+    [DllImport("DLL_CV")]
+    private static extern IntPtr getCamera();
+    [DllImport("DLL_CV")]
+    private static extern void getCameraTexture(IntPtr camera, IntPtr data, int width, int height);
+
+    private IntPtr camera_;
+    private Texture2D texture_;
+    private Color32[] pixels_;
+    private GCHandle pixels_handle_;
+    private IntPtr pixels_ptr_;
+    void Start()
+    {
+        camera_ = getCamera(); //NativePlugin
+        texture_ = new Texture2D(960, 540, TextureFormat.RGBA32, false);
+        pixels_ = texture_.GetPixels32();
+        pixels_handle_ = GCHandle.Alloc(pixels_, GCHandleType.Pinned);
+        pixels_ptr_ = pixels_handle_.AddrOfPinnedObject();
+        GetComponent<MeshRenderer>().material.mainTexture = texture_;
+    }
+    void Update()
+    {
+        getCameraTexture(camera_, pixels_ptr_, texture_.width, texture_.height);//NativePlugin
+        texture_.SetPixels32(pixels_);
+        texture_.Apply();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NativePlugin_OpenCV
+ 
